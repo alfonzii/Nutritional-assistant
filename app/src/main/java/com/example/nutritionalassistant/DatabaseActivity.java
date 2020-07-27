e1de0f0bc378ef4340a540eaf1da76bfbbb13949
Alfonzi <semicco2@gmail.com>
2020-07-27 04:27:43 +0200

    Third commit

--- app/src/main/java/com/example/nutritionalassistant/DatabaseActivity.java
+++ app/src/main/java/com/example/nutritionalassistant/DatabaseActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 
 import java.util.ArrayList;
 
@@ -14,18 +16,27 @@ public class DatabaseActivity extends AppCompatActivity {
 
     //vars
     private ArrayList<String> mNames = new ArrayList<>();
-    private NutritionDbHelper dbHelper = NutritionDbHelper.getInstance(this);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
         Log.d(TAG, "onCreate: started.");
+
+        NutritionDbHelper dbHelper = NutritionDbHelper.getInstance(this);
 
         mNames = (ArrayList<String>)dbHelper.getFoodNamesQuery(this, dbHelper.getReadableDatabase(), true);
         initRecyclerView();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_database, menu);
+        return true;
+    }
 
     private void initRecyclerView(){
         Log.d(TAG, "initRecyclerView: init recyclerview.");
